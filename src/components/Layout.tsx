92f50b32ccc21f07522c60d3aea08c663b593328
Kamal Alshinawi <kamalalshinawi@gmail.com>
2026-09-14 15:25:22 +0300

    feat(layout): add top contact banner with contact details and social links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
+import { TopBanner } from "./TopBanner";
+
 export const Layout = (): React.JSX.Element => (
-  <div className="flex min-h-screen flex-col overflow-x-hidden">
+  <div className="flex min-h-screen flex-col overflow-x-hidden bg-white text-black">
+    <TopBanner />
     <Navbar />
     <main className="mx-auto w-full max-w-6xl flex-1 px-6 py-12">
       <Outlet />
